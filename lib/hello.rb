bd754229a68e68874cec543854721ee66a3b81cf
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-03-21 06:21:39 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,5 @@
 def hello_t(array)
+  if block_given?
 i = 0 
   while i < array.length 
   yield(array[i])
@@ -6,9 +7,11 @@ i = 0
  end
  
  array
-else
+ else
   puts "Hey! No block was given!"
 end
+end
+
 
 
 
